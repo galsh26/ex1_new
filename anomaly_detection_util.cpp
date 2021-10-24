42b87268f010466b3b0fd0115ca04b27be24592d
galsh26 <galsh26@gmail.com>
2021-10-24 16:31:23 +0300

    changed abs and pow

--- anomaly_detection_util.cpp
+++ anomaly_detection_util.cpp
@@ -8,6 +8,8 @@
 #include <cmath>
 #include "anomaly_detection_util.h"
 
+using namespace std;
+
 // returns the mean of X
 float mean(const float* x, int size) {
     float sum = 0;
@@ -19,18 +21,18 @@ float mean(const float* x, int size) {
 
 // returns the variance of X
 float var(float* x, int size) {
-    float x_mean, var = 0;
+    float x_mean = 0, var = 0;
     x_mean = mean(x, size);
     for (int i = 0; i < size; ++i) {
-        var += powf(x[i], 2);
+        var = var + (x[i] * x[i]);
     }
     var = var / size;
-    return (var - powf(x_mean, 2));
+    return (var - (x_mean * x_mean));
 }
 
 // returns the covariance of X and Y
 float cov(float* x, float* y, int size) {
-    float x_mean, y_mean, var = 0;
+    float x_mean = 0, y_mean = 0, var = 0;
     x_mean = mean(x, size);
     y_mean = mean(y, size);
     for (int i = 0; i < size; ++i) {
@@ -41,14 +43,12 @@ float cov(float* x, float* y, int size) {
 
 // returns the Pearson correlation coefficient of X and Y
 float pearson(float* x, float* y, int size) {
-    float x_var, y_var, xy_cov;
+    float x_var = 0, y_var = 0, xy_cov = 0;
     x_var = var(x, size);
     y_var = var(y, size);
     xy_cov = cov(x, y, size);
     return xy_cov / (sqrtf(x_var) * sqrtf(y_var));
 }
-
-
 
 // performs a linear regression and return s the line equation
 Line linear_reg(Point** points, int size) {
@@ -70,14 +70,20 @@ Line linear_reg(Point** points, int size) {
 float dev(Point p,Point** points, int size) {
     Line l = linear_reg(points, size);
     float expected = l.f(p.x);
-    return std::abs(expected - p.y);
+    float res = expected - p.y;
+    if (res < 0) {
+        res = res * (-1);
+    }
+    return res;
 }
 
 // returns the deviation between point p and the line
 float dev(Point p,Line l) {
     float expected = l.f(p.x);
-    return std::abs(expected - p.y);
+    float res = expected - p.y;
+    if (res < 0) {
+        res = res * (-1);
+    }
+    return res;
 }
-
-using namespace std;
 
