1442cc0fac4b1f30c043cb215d76630a2a6620fc
amitzakai <amitzakai@hotmail.com>
2021-11-11 03:18:06 -0800

    ex2 try1

--- timeseries.h
+++ timeseries.h
@@ -4,3 +4,19 @@
  * Author: Gal Schlifstein, id: 209492925
  * Author: Amit Zakai, id: 318654167
  */
+
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+class timeseries {
+private:
+    map<const string, vector<float>>;
+
+public:
+    timeseries(string file_name);
+    feature_by_time(string typeName, float time);
+};
