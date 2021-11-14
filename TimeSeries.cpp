14ac9e511d3f96670fcb714104e2037cd249b219
amitzakai <amitzakai@hotmail.com>
2021-11-14 10:42:29 -0800

    ex2 finished constructor

--- TimeSeries.cpp
+++ TimeSeries.cpp
@@ -14,3 +14,31 @@
 #include <sstream>
 
 using namespace std;
+
+TimeSeries::TimeSeries(const char *file_name) {
+    ifstream myFile(file_name);
+    if(!myFile.is_open()) throw std::runtime_error("Error opening file");
+    string line, column;
+    float value = 0;
+    if (myFile.good()) {
+        getline(myFile, line);
+        stringstream strs(line);
+        while (getline(strs, column, ',')) {
+            this->tableVectors.push_back({column, vector<float>{}});
+        }
+    }
+    while (getline(myFile, line)) {
+        stringstream strs(line);
+        int index = 0;
+        while (strs >> value) {
+            this->tableVectors.at(index).second.push_back(value);
+            if (strs.peek() == ',') {
+                strs.ignore();
+            }
+            index++;
+        }
+    }
+    myFile.close();
+}
+
+
--- TimeSeries.h
+++ TimeSeries.h
@@ -16,39 +16,14 @@
 #include <fstream>
 #include <sstream>
 
-//using namespace std;
-
 class TimeSeries {
     private:
-        std::map<const std::string ,std::vector<float>> mapTimeSeries;
+        std::vector<std::pair<std::string, std::vector<float>>> tableVectors;
         std::vector<float> values;
         std::vector<std::string> colNames;
         int rowSize;
 
     public:
-        TimeSeries(const char* file_name) {
-            std::ifstream myFile(file_name);
-            if(!myFile.is_open()) throw std::runtime_error("can't open file");
-            std::string line, column;
-            float value = 0;
-            std::pair<std::string, std::vector<float>> temp;
-            if (myFile.good()) {
-                getline(myFile, line);
-                std::stringstream stream1(line);
-                while (getline(stream1, column, ',')) {
-                    temp.first = column;
-                    this->mapTimeSeries.insert(temp);
-                }
-                // I think that we need to insert to colNames the info, I debbuged and it's empty
-            }
-            int i = 0;
-            this->rowSize = colNames.size();
-            while (getline(myFile, line)) {
-                std::stringstream stream2(line);
-                while (stream2 >> value) {
-                    //this->mapTimeSeries.insert(this->colNames.at(i), temp.second.push_back(value));
-                }
-            }
-        }
+        TimeSeries(const char* file_name);
         float feature_by_time(std::string typeName, float time);
 };
